adb922efbcf0d13f70911514d8505b1beea3cffa
Rajalakshmi <sec23cj001@sairamtap.edu.in>
2025-03-23 22:49:45 +0530

    navbar

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact for more information</div>
+    <div align="center">Contact for more information</div>
   )
 }
 
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div align="center">Services</div>
   )
 }
 
